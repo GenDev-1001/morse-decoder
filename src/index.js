67892fc462830a645f8ce5632a24118d883e5121
GenDev-1001 <evgenii.ubileii@bk.ru>
2022-01-19 19:53:03 +0300

    feat: realize function morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,15 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    let result = []
+    let resultString = '';
+    for (let i = 0; i < expr.length; i += 10) result.push(expr.slice(i, i + 10));
+    result
+        .map(codeItem => codeItem.join('').match(/(11|10)/gm))
+        .map(decodeItem => (decodeItem === null) ? " " : decodeItem.map(morseItem => (+morseItem === 10) ? '.' : '-'))
+        .map(symbolItem => (symbolItem === ' ') ? resultString += ' ' : resultString += MORSE_TABLE[[...symbolItem].join('').toString()])
+    return resultString;
 }
 
 module.exports = {
